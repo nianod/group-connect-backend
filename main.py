95b73fadccef9c9a6c5ca80682e1d1064d6a49d8
nianod <arnoldkk422@gmail.com>
2025-11-13 16:36:50 +0300

    cors fixed

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins = [
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
         "https://group-connect-gamma.vercel.app"
     ],
     allow_credentials=True,
